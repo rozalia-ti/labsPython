9b77c3cc137d49574709c7e20ca5410990650ea7
Роза Тихонова <rozatih29@gmail.com>
2025-09-06 10:32:17 +0300

    Delete new.py

--- new.py
+++
@@ -1,9 +0,0 @@
-from test import two_sum
-import unittest
-
-class TestMySolution(unittest.TestCase):
-    def test_simple(self):
-        self.assertEqual(two_sum([0, 6], 6), [0, 1])
-
-if __name__ ==  '__main__':
-    unittest.main()
