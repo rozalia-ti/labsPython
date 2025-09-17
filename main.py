6cab562479fb7e3b6b6986a88c4d293d0b6e0a2d
Roza <rozatih29@gmail.com>
2025-09-17 11:26:59 +0300

    поправки к первой лабораторной работе

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-nums = [0, 323] # вот эти значения вообще никак не влияют на результаты теста, это всего ллишь инициализация
-target = 6.4
+nums = [3, 4] # вот эти значения вообще никак не влияют на результаты теста, это всего ллишь инициализация
+target = 42
 def two_sum(nums, target):
     if (all(isinstance(x, int) for x in nums)) and (isinstance(target, int)): #проверка на целочисленные значения
         for _o1 in range(len(nums)- 1):
@@ -9,4 +9,3 @@ def two_sum(nums, target):
                     return(res)
     print(two_sum(nums,target))
     # assert(two_sum(nums,target) == [0, 1], "Проверка провалена") #вот эту строчку я комментирую потому что тесты происходят в файле test.py 
-   
--- test.py
+++ test.py
@@ -2,9 +2,16 @@ from main import two_sum
 import unittest
 
 class TestMySolution(unittest.TestCase):
-    def test_simple(self):
-        self.assertEqual(two_sum([0, 6.3], 6.3), [0, 1]) #здесь вводить значения списка и таргета
+    def test_first(self):
+        self.assertEqual(two_sum([2,7,11,15], 9), [0, 1]) # этот тест должен выполняться хорошо без ошибок
+
+    def test_second(self):
+        self.assertEqual(two_sum([3, 2, 4], 6), [1, 2]) #этот тест тоже должен работать без ошибок
+
+    def test_third(self):
+        self.assertEqual(two_sum([3,3], 6), [0, 1]) # и это тоже должно быть верным
+    def test_error_first(self):
+            self.assertEqual(two_sum([0, 6.3], 6.3), [0, 1]) #здесь должна произойти поломка, т.к. число с плавающей точкой, а не int, как оно должно быть в условии.
     
 if __name__ ==  '__main__':
     unittest.main()
-#P.S. я сделала только один тест потому что не вижу смысла тестировать что-то ещё, например, действительно ли входными параметрами являются целочисленные значения, потому что этот параметр у нас и проверяет тест assertEqual, потому что в функцию two_sum я добавила условие которое предотвращает иные случаи. 
