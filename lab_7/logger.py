e1b6024e9ebeea0fa6fa6604ad62fbf9c1469967
Roza <rozatih29@gmail.com>
2025-11-28 14:23:54 +0300

    added logger but not all

--- lab_7/logger.py
+++ lab_7/logger.py
@@ -1,14 +1,24 @@
 import sys
 
-#поток вывода в консоль
+
 def logger(func=None, *, handle=sys.stdout):
-    def wrapper():
-        handle.write(f"before function launch\n")
-        func()
-        handle.write('after function launch\n')
+    def wrapper(*args, **kwargs):
+        match handle:
+            case sys.stdout:
+                handle.write(f"before function launch\n")
+                res = func(*args, **kwargs)
+                handle.write(f"result = {res} \n")
+                handle.write('after function launch\n')
+                return res
     return wrapper
 
-# @logger(handle)      
-# def current(handle):
-#     handle.write("24\n");
-# current(handle = sys.stdout)
+# @logger      
+# def current(*args, **kwargs):
+#     return (f"args:{args} \n kwargs: {kwargs} \n");
+# current(1, 'i wanna be ypur slave', True, omega = 'PSG', name = 'Roza', age = 18)
+
+@logger
+def sum(a, b):
+    return a + b;
+sum(3, 7)
+
