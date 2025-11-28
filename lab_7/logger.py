414625477897f58ab73efe4769793c451b40b0ed
Roza <rozatih29@gmail.com>
2025-11-28 12:51:24 +0300

    adding lab_7 folder after deleting

--- lab_7/logger.py
+++ lab_7/logger.py
@@ -1,20 +1,14 @@
 import sys
-from functools import wraps
 
-
+#поток вывода в консоль
 def logger(func=None, *, handle=sys.stdout):
-    def wrapper(func):
-        @wraps(func)
-        def logger_stdout(*args, **kwargs):
-            print()
-            func(*args, **kwargs)
-            
-        return logger_stdout
+    def wrapper():
+        handle.write(f"before function launch\n")
+        func()
+        handle.write('after function launch\n')
     return wrapper
 
-
-@logger()
-def x(y=None):
-    print(y)
-
-x(y=1)
+# @logger(handle)      
+# def current(handle):
+#     handle.write("24\n");
+# current(handle = sys.stdout)
